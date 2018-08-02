4f95f7fa4753b2d1d6d2234ff1266d81e4d91209
Victor Hugo do Nascimento <38699623+Viictorvhn@users.noreply.github.com>
2018-08-02 17:28:21 -0300

    dasffe

---
+++ ExemploMVC02/ExemploMVC02/Controllers/RecrutadoraController.cs
@@ -0,0 +1,31 @@
+﻿using ExemploMVC02.Models;
+using ExemploMVC02.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploMVC02.Controllers
+{
+    public class RecrutadoraController : Controller
+    {
+        // GET: Recrutadora
+        public ActionResult Index()
+        {
+            List<Recrutadora> recrutadoras = new RecrutadoraRepository().ObterTodos();
+            ViewBag.Recrutadoras = recrutadoras;
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExemploMVC02/ExemploMVC02/Repository/RecrutadoraRepository.cs
@@ -0,0 +1,67 @@
+﻿using ExemploMVC02.Database;
+using ExemploMVC02.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ExemploMVC02.Repository
+{
+    public class RecrutadoraRepository
+    {
+        public List<Recrutadora> ObterTodos()
+        {
+            List<Recrutadora> recrutadoras = new List<Recrutadora>();
+            SqlCommand command = new BancoDados().ObterConexao();
+            command.CommandText = "SELECT id, nome, cpf, tempo_empresa, salario FROM recrutadoras";
+            DataTable tabela = new DataTable();
+            tabela.Load(command.ExecuteReader());
+            foreach (DataRow linha in tabela.Rows) 
+            {
+                Recrutadora recrutadora = new Recrutadora()
+                {
+                    Id = Convert.ToInt32(linha[0].ToString()),
+                    Nome = linha[1].ToString(),
+                    CPF = linha[2].ToString(),
+                    TempoEmpresa = Convert.ToByte(linha[3].ToString()),
+                    Salario = Convert.ToDecimal(linha[4].ToString())
+                };
+                recrutadoras.Add(recrutadora);
+            }
+            return recrutadoras;
+        }
+
+        public int Cadastrar(Recrutadora recrutadora)
+        {
+            SqlCommand command = new BancoDados().ObterConexao();
+
+            command.CommandText = @"INSERT INTO recrutadoras (nome, slario, cpf, tempo_empresa) 
+            OUTPUT INSERTED.ID
+            VALUES (@NOME, @SALARIO, @CPF, @TEMPO_EMPRESA)";
+            command.Parameters.AddWithValue("@NOME", recrutadora.Nome);
+            command.Parameters.AddWithValue("@SALARIO", recrutadora.Salario);
+            command.Parameters.AddWithValue("@CPF", recrutadora.CPF);
+            command.Parameters.AddWithValue("@TEMPO_EMPRESA", recrutadora.TempoEmpresa);
+            int id = Convert.ToInt32(command.ExecuteScalar().ToString());
+            return 0;
+        }
+
+        public bool Alterar(Recrutadora recrutadora)
+        {
+            return false;
+        }
+
+        public bool Excluir(int id)
+        {
+            return false;
+        }
+
+        public Recrutadora ObterPeloId(int id)
+        {
+            Recrutadora recrutadora = null;
+            return recrutadora;
+        }
+    }
+}
